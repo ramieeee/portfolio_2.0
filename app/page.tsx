e8ee8dec1ad6c2643e2971bf221c5cd5b0913f94
ramieeee <rambotty@gmail.com>
2025-01-01 23:11:00 +0900

    Add font

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,8 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <div className={styles.page}>
-      <h1>Hello World</h1>
+      <h1 className={"font-suitBold text-9xl"}>Hello World</h1>
+      <h1 className={"font-suit text-9xl"}>Hello World</h1>
     </div>
   );
 }

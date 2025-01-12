d3b953d171d7a77f63d30edad62b223573989bfd
ramieeee <rambotty@gmail.com>
2025-01-12 11:09:35 +0900

    Fix project card

--- components/molecules/ProjectCard.tsx
+++ components/molecules/ProjectCard.tsx
@@ -8,7 +8,11 @@ interface ProjectCard {
 
 export default function ProjectCard({ project }: ProjectCard) {
   return (
-    <div className="w-[350px] h-[500px] p-2 border-[1px] border-color-main">
+    <div
+      className="flex flex-col w-[350px] h-[500px] p-2 border-[1px]
+      border-color-main cursor-pointer hover:bg-color-secondary
+      transition-all duration-200 overflow-hidden gap-6"
+    >
       {/* img */}
       <div className="w-full h-[200px] flex justify-center items-center mx-auto">
         {project?.images.length > 0 ? (
@@ -22,41 +26,34 @@ export default function ProjectCard({ project }: ProjectCard) {
             <Image
               src={PhotoSvg}
               alt="No Image"
-              width={100}
-              height={100}
-              style={{ width: "40%", height: "40%" }}
+              width={50}
+              height={50}
+              style={{ width: "20%", height: "20%" }}
             />
             <div className="text-color-main font-suitBold">
               No image available
             </div>
           </div>
         )}
-        {/* {project?.images.length > 0 ? (
-          <Image
-            src={`/${project?.images[0]}`}
-            alt="Project Image"
-            width={100}
-            height={100}
-            style={{ width: "auto", height: "100%" }}
-          />
-        ) : (
-          <div className="flex flex-col justify-center items-center">
-            <Image
-              src={PhotoSvg}
-              alt="No Image"
-              width={100}
-              height={100}
-              style={{ width: "40%", height: "40%" }}
-            />
-            <div>No image</div>
-          </div>
-        )} */}
       </div>
 
       {/* 제목, 기간 */}
-      <div></div>
+      <div className="flex flex-col">
+        <div className="font-suitBold text-size-subtitle text-color-highlight">
+          {project.title}
+        </div>
+        <div>
+          <div className="font-suitBold text-size-body text-color-secondary">
+            {project.organization}
+          </div>
+          <div className="font-suitBold text-size-body text-color-secondary">{`${project.time.from.year}.${project.time.from.year} - ${project.time.until.year}.${project.time.until.year}`}</div>
+        </div>
+      </div>
 
       {/* 설명 */}
+      <div className="flex font-suitBold text-size-body text-color-main">
+        {project.description}
+      </div>
     </div>
   );
 }

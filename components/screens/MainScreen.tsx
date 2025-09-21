52527954349f459dbf53eccafcceaaef5eb4c4a7
ramieeee <rambotty@gmail.com>
2025-09-21 16:51:04 +0900

    Fix name style

--- components/screens/MainScreen.tsx
+++ components/screens/MainScreen.tsx
@@ -10,33 +10,38 @@ import LinkedinLogo from "@/assets/linkedinLogo.svg";
 export default function MainScreen() {
   return (
     <Template className="h-screen flex items-center">
-      <div className="flex flex-col gap-20 w-full">
-        <div className="flex flex-row h-[320px] gap-6">
-          <div className="w-1/2 flex flex-col gap-3 h-full justify-end">
-            <div className="text-right font-suitBold text-size-title text-color-main">
-              Ramhee Yeon
+      <div className="flex flex-col gap-10 sm:gap-20 w-full">
+        <div className="flex flex-col sm:flex-row h-auto sm:h-[320px] gap-6">
+          <div className="w-full sm:w-1/2 flex flex-col gap-3 h-full justify-center sm:justify-end order-2 sm:order-1">
+            <div className="flex flex-row justify-center gap-2 sm:flex-col sm:justify-between sm:h-20">
+              <div className="text-center sm:text-right font-suitBold text-xl sm:text-size-title text-color-main">
+                Ramhee
+              </div>
+              <div className="text-center sm:text-right font-suitBold text-xl sm:text-size-title text-color-main">
+                Yeon
+              </div>
             </div>
-            <div className="flex flex-col text-right">
-              <div className="font-suitBold text-size-subtitle text-color-main">
+            <div className="flex flex-col text-center sm:text-right">
+              <div className="font-suitBold text-base sm:text-size-subtitle text-color-main">
                 머신러닝 엔지니어,
               </div>
-              <div className="font-suitBold text-size-subtitle  text-color-main">
+              <div className="font-suitBold text-base sm:text-size-subtitle text-color-main">
                 인공지능 석학 전공
               </div>
             </div>
-            <div className="flex flex-col text-right">
-              <div className="font-suitBold text-size-body text-color-secondary">
+            <div className="flex flex-col text-center sm:text-right">
+              <div className="font-suitBold text-sm sm:text-size-body text-color-secondary">
                 매 순간 변화하는 IT 세계에서 새 기술을 익히고
               </div>
-              <div className="font-suitBold text-size-body text-color-secondary">
+              <div className="font-suitBold text-sm sm:text-size-body text-color-secondary">
                 새로운 아이디어를 통해 나의 방식대로 프로그래밍
               </div>
-              <div className="font-suitBold text-size-body text-color-secondary">
+              <div className="font-suitBold text-sm sm:text-size-body text-color-secondary">
                 하는 것은 제게 언제나 기쁨을 느끼게 해줍니다.
               </div>
             </div>
             {/* 아이콘들 */}
-            <div className="flex flex-row gap-3 justify-end">
+            <div className="flex flex-row gap-3 justify-center sm:justify-end">
               <Link
                 href="https://www.linkedin.com/in/ramieeee"
                 target="__blank"
@@ -48,7 +53,7 @@ export default function MainScreen() {
               </Link>
             </div>
           </div>
-          <div className="flex w-1/2 h-full items-end">
+          <div className="flex w-full sm:w-1/2 h-auto sm:h-full items-center sm:items-end justify-center order-1 sm:order-2">
             <div className="flex align-bottom p-3 border-color-main border-[1px]">
               <Image
                 className="grayscale hover:grayscale-0 cursor-pointer"
@@ -56,7 +61,7 @@ export default function MainScreen() {
                 alt="myPhoto"
                 style={{
                   height: "auto",
-                  width: 240,
+                  width: 180,
                   transition: "0.5s",
                 }}
               />
@@ -65,14 +70,14 @@ export default function MainScreen() {
         </div>
 
         {/* 아래 quatation */}
-        <div className="flex flex-col w-full items-center">
-          <div className="flex flex-col w-7/12">
-            <div className="font-suitBold text-color-main text-size-subtitle align-middle">
+        <div className="flex flex-col w-full items-center px-4">
+          <div className="flex flex-col w-full sm:w-7/12">
+            <div className="font-suitBold text-color-main text-base sm:text-size-subtitle text-center sm:text-left">
               {
                 " The most dangerous phrase in the language is, 'We've always done it this way.' "
               }
             </div>
-            <div className="font-suitBold text-color-secondary text-size-subtitle text-right">
+            <div className="font-suitBold text-color-secondary text-sm sm:text-size-subtitle text-center sm:text-right mt-2">
               by Grace Hopper
             </div>
           </div>
